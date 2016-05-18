afed8515b8c4fa8273aecace4ce7dbbea93b2160
Andrew Vetter <Ajvetter28@gmail.com>
2016-05-17 22:13:37 -0500

    Update BattleSystem.java
    I couldn't open the files at home because eclipses is dumb. I edited the files in github. They should work.

--- GameTime/src/BattleSystem.java
+++ GameTime/src/BattleSystem.java
@@ -11,13 +11,13 @@ public class BattleSystem {
 		double start = Math.random(); 
 		if (start <.5 ){
 			//if random number is in bottom half of what it could be wizard 1 goes first
-			String temp = w1.castSpell(w2);
+			String temp = castSpell(w1,w2);
 			testerama.ActionBox(temp);
 			testerama.update();
 		}
 		else{
 			//if not then wizard 2 goes first
-			String temp2 = w2.castSpell(w1);
+			String temp2 = castSpell(w2,w1);
 			testerama.ActionBox(temp2);
 			testerama.update();
 		}
@@ -46,7 +46,7 @@ public class BattleSystem {
 			//w1.castSpell(w2);
 			
 			if (w1.getHP() > 0){
-				String temp3 = w1.castSpell(w2);
+				String temp3 = castSpell(w1,w2);
 				testerama.ActionBox(temp3);
 				testerama.update();
 			}
@@ -65,7 +65,7 @@ public class BattleSystem {
 
 			//w2.castSpell(w1);
 			if (w2.getHP() > 0) {
-				String temp4 = w2.castSpell(w1);
+				String temp4 = castSpell(w2,w1);
 				testerama.ActionBox(temp4);
 				testerama.update();
 			}
@@ -83,5 +83,88 @@ public class BattleSystem {
 			//cool down implementation
 		}
 		testerama.AnnounceWinner(w1,w2);
+	}
+	
+	public String castSpell(abstractWizard wCaster, abstractWizard wRecieve){
+		// NEED TO IMPLEMENT check for disarmed or other inhibitors
+		
+		if (!wCaster.getCastAbility())
+		{
+			//they can't cast a spell if they are disarmed
+			System.out.println(wCaster.getName() + " is disarmed, they cannot cast a spell");
+			//TEMPORARY, ONLY TO TEST WITHOUT FIXING DISARM
+			wCaster.setCastAbility(true);
+			return wCaster.getName() + " is disarmed, they cannot cast a spell";
+		}
+		
+		Spell chosenSpell = wCaster.chooseSpell();
+		
+		//set value of current spells cool down for use in BattleSystem
+		wCaster.setCurrentSpellCooldown(chosenSpell.getCoolDown()); 
+		System.out.println(wCaster.getName() +" casts "+ chosenSpell.getName());
+		//check if spell is targeting self
+		if (chosenSpell.getSelfTargeting()){
+			//check for protective spell
+			if (chosenSpell.getProtecter()){
+				System.out.println(wCaster.getName() + " is now warded");
+				wCaster.setWarded(true);
+				return wCaster.getName() +" casts "+ chosenSpell.getName() + "\n" + wCaster.getName() + " is now warded";
+			}
+			//assuming that if it isn't a ward it heals the wizard 
+			//Will be changed if more self-targeting spells are implemented
+			
+			else{
+				//May need to be changed, 
+				//regenerates HP based on Spell and Intellect
+				
+				int healingAmount = chosenSpell.getHealer()*wCaster.getIntellect();
+								
+				if(wCaster.getHP() + healingAmount > wCaster.getHP())
+				{
+					healingAmount = wCaster.maxHP-this.HP;
+					this.HP = this.maxHP;
+					 
+				}
+				else{
+					this.setHP(this.HP + healingAmount);
+				}
+					
+				System.out.println(this.getName() + " healed for " + healingAmount + " health" );
+				return this.getName() +" casts "+ chosenSpell.getName() + "\n" + this.getName() + " healed for " + healingAmount + " health" ;
+			}
+			
+		}
+		else if (wRecieve.getWarded()){
+			System.out.println(chosenSpell.getName() + " was blocked");			
+			this.setWarded(false);
+			return this.getName() +" casts "+ chosenSpell.getName() + "\n" + chosenSpell.getName() + " was blocked";
+		}
+		//This code exists under the assumption that disarm does nothing but disarm
+		else if (chosenSpell.getDisarm())
+		{
+			//disarm will prevent opponent from casting spell
+			//unless they are warded
+			StatusEffect disarm  = new StatusEffect();
+				System.out.println(wRecieve.getName()+ " was disarmed");
+				wRecieve.setCastAbility(false);
+				this.currentEffects.add(disarm);
+			return this.getName() +" casts "+ chosenSpell.getName() + "\n" + wRecieve.getName()+ " was disarmed";
+			}
+						
+		
+		//assuming that if it isn't self-targeting or disarming, its a damaging attack
+		
+		
+		else{
+			//calculate damage based on wizards intellect and spell power
+			int damage = (int)(this.getIntellect()*.01) * chosenSpell.getPower();
+			System.out.println(chosenSpell.getName() + " dealt "+ damage + " damage");
+			//current way of modifying opponents health
+			wRecieve.setHP(wRecieve.HP- damage);
+			System.out.println(wRecieve.getName() + " has " +wRecieve.getHP() + " health left");
+		 
+		return this.getName() +" casts "+ chosenSpell.getName() + "\n" + chosenSpell.getName() + " dealt "+ damage + " damage" + "\n" + wRecieve.getName() + " has " +wRecieve.getHP() + " health left";
+		}
+	
 	}
 }
