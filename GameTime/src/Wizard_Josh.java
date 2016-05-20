aca512c83800217dd6355fd87402fcbeba8074a6
Joshua Bultman <joshuab1239@gmail.com>
2016-05-20 09:16:31 -0500

    added condition for spell

--- GameTime/src/Wizard_Josh.java
+++ GameTime/src/Wizard_Josh.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 public class Wizard_Josh extends abstractWizard {
 
 	public Wizard_Josh(ArrayList<Spell> spellBook) {
-		
+
 		super("Josh", 250, 250, 200, spellBook);
-		
+
 		double random = Math.random();
-		int Intelect = 0;
-		int HP = 0;
-		int Attunement = 0;
+		int Intelect = 250;
+		int HP = 250;
+		int Attunement = 200;
 		if(random <.6){
 			Intelect = 500;
 			Attunement = 250;
@@ -22,5 +22,18 @@ public class Wizard_Josh extends abstractWizard {
 		this.setHP(HP);
 		this.setMaxHP(HP);
 	}
+
+	@Override
+	public Spell chooseSpell() {
+		double healthPercent = (this.getHP()/this.GetMaxHP());
+		if(healthPercent < 0.38){
+			return this.getSpellBook().get(10);
+		}
+		else{
+			int spellBookSize = this.getSpellBook().size();
+			return this.getSpellBook().get((int)(Math.random() * spellBookSize));
+		}
+	}
+
 
 }
